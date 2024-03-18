dbfa90725b0f8661121e4f3bbfbe9e908689ce45
Neelanchal Joshi <neelanchaljoshi@gmail.com>
2024-03-18 14:12:42 +0100

    my solutions for functions2 exercise v1

--- exercises/functions2.py
+++ exercises/functions2.py
@@ -12,3 +12,19 @@ def getListofRawFiles(dir_path):
         for raw_file in all_raw_files:
             raw_files.append(os.path.join(mypath, raw_file))
     return raw_files
+
+'''
+My Solution below
+'''
+def get_raw_file_names_in_dir(dir_path: str) -> list:
+    return [fileName for fileName in os.listdir(dir_path) if fileName.endswith(".raw")]
+
+def get_raw_file_paths(dir_path: str) -> list:
+    return [os.path.join(dir_path, fileName) for fileName in get_raw_file_names_in_dir(dir_path)]
+
+def getListofRawFiles(dir_path: list) -> list:
+    dir_list = [x[0] for x in os.walk(dir_path)]
+    raw_files = []
+    for dir_path in dir_list:
+        raw_files += get_raw_file_paths(dir_path)
+    return raw_files
