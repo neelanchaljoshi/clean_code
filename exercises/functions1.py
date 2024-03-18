85cbd1040d07259d75071bc783e251edc93d2221
Neelanchal Joshi <neelanchaljoshi@gmail.com>
2024-03-18 13:36:00 +0100

    my solution for the refactoring problem v1

--- exercises/functions1.py
+++ exercises/functions1.py
@@ -6,6 +6,10 @@ class FunctionExample1:
     ## Original function that we have seen in the slides
     """
 
+    def update_time(self, new_time: datetime) -> list:
+        self.current_time = new_time
+        return self
+
     def dir_path(self, start: datetime) -> str:
         # Dummy Function for demonstration purposes
         return start.strftime('%Y_%M_%D_%H')
@@ -19,3 +23,14 @@ class FunctionExample1:
                 paths.append(self.dir_path(interim))
                 interim += one_hour
         return paths
+    
+    # My Solution
+    def get_data_path_list(self, start_time: datetime, stop_time: datetime = None) -> list:
+        path_list = [self.dir_path(start_time)]
+        self.update_time(start_time)
+        if start_time == stop_time:
+            return path_list
+        while self.time <= stop_time:
+            path_list.append(self.dir_path(self.current_time))
+            self.update_time(self.current_time + timedelta(hours=1))
+        return path_list
